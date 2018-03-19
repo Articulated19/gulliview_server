e5f4e3be36e973fc7e4b4b0de1e9354469b13ea5
Sebastian Nilsson <sebbehebbe@users.noreply.github.com>
2018-03-19 10:02:25 +0100

    Implemented socket server (#1)
    * Socket server now working for the gulliview ros node
    
    * Now collecting data about what camera is capturing the gv tag

---
+++ scripts/gv_socket_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import SocketServer
+import rospy
+import json
+import os
+from custom_msgs.msg import *
+
+class GulliViewServer:
+
+    def __init__(self):
+        rospy.init_node('gulliview', anonymous=True)
+        self.gv_position_publisher = rospy.Publisher('gv_positions', GulliViewPositions, queue_size=10)
+
+
+    def handlePositionUpdate(self, data):
+        msg = GulliViewPositions()
+
+        msg.p1.x = data.get('x1')
+        msg.p1.y = data.get('y1')
+        msg.cameraid = data.get('cameraId1')
+        msg.cameraid1 = data.get('cameraId1')
+        msg.cameraid2 = data.get('cameraId2')
+        msg.tagid1 = data.get('tagid1')
+        msg.p2.x = data.get('x2')
+        msg.p2.y = data.get('y2')
+        msg.tagid2 = data.get('tagid2')
+
+        self.gv_position_publisher.publish(msg)
+
+class GVSocketServer(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        global gv
+        data = self.request[0].strip()
+        data = json.loads(data.decode())
+
+        gv.handlePositionUpdate(data)
+        print data
+
+if __name__ == "__main__":
+    HOST, PORT = os.environ['GV_NETWORK_IP'], 2121
+
+    gv = GulliViewServer()
+    server = SocketServer.UDPServer((HOST, PORT), GVSocketServer)
+    server.serve_forever()
+    rospy.spin()
