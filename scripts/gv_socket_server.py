086cbf44dcdf456f40c3a25ae7a36cb767ac1938
Lars Niklasson <larsniklasson94@gmail.com>
2018-04-18 19:46:57 +0200

    Fixed precision

--- scripts/gv_socket_server.py
+++ scripts/gv_socket_server.py
@@ -8,19 +8,20 @@ from custom_msgs.msg import *
 class GulliViewServer:
 
     def __init__(self):
+        print "hi"
         rospy.init_node('gulliview', anonymous=True)
         self.gv_position_publisher = rospy.Publisher('gv_positions', GulliViewPositions, queue_size=10)
 
 
     def handlePositionUpdate(self, data):
+        print data
         msg = GulliViewPositions()
 
         msg.p1.x = data.get('x1')
         msg.p1.y = data.get('y1')
-        msg.cameraid = data.get('cameraId1')
-        msg.cameraid1 = data.get('cameraId1')
-        msg.cameraid2 = data.get('cameraId2')
+        msg.cameraid = data.get('cameraid')
         msg.tagid1 = data.get('tagid1')
+        
         msg.p2.x = data.get('x2')
         msg.p2.y = data.get('y2')
         msg.tagid2 = data.get('tagid2')
@@ -38,7 +39,7 @@ class GVSocketServer(SocketServer.BaseRequestHandler):
         print data
 
 if __name__ == "__main__":
-    HOST, PORT = os.environ['GV_NETWORK_IP'], 2121
+    HOST, PORT = "localhost", 2121
 
     gv = GulliViewServer()
     server = SocketServer.UDPServer((HOST, PORT), GVSocketServer)
